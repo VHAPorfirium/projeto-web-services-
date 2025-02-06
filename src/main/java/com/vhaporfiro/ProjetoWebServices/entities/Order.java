0999ea4104ac3f92eccc563874a61a2a87eb53d9
Victor Hugo <vhaporfiro@gmail.com>
2025-02-06 12:15:44 -0300

    Enums

--- src/main/java/com/vhaporfiro/ProjetoWebServices/entities/Order.java
+++ src/main/java/com/vhaporfiro/ProjetoWebServices/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.vhaporfiro.ProjetoWebServices.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,7 +17,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable {
+public final class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -27,15 +28,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id") 
     private User client;
 
     public Order() {}   
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -63,6 +67,16 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+    
     @Override
     public int hashCode() {
         int hash = 5;
