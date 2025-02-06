93c38652e1d7d782149780bceeda6df5bd598306
Victor Hugo <vhaporfiro@gmail.com>
2025-02-06 12:42:53 -0300

    Category

--- src/main/java/com/vhaporfiro/ProjetoWebServices/config/TestConfig.java
+++ src/main/java/com/vhaporfiro/ProjetoWebServices/config/TestConfig.java
@@ -2,21 +2,27 @@ package com.vhaporfiro.ProjetoWebServices.config;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.vhaporfiro.ProjetoWebServices.entities.Category;
 import com.vhaporfiro.ProjetoWebServices.entities.Order;
 import com.vhaporfiro.ProjetoWebServices.entities.User;
 import com.vhaporfiro.ProjetoWebServices.entities.enums.OrderStatus;
+import com.vhaporfiro.ProjetoWebServices.repositories.CategoryRepository;
 import com.vhaporfiro.ProjetoWebServices.repositories.OrderRepository;
 import com.vhaporfiro.ProjetoWebServices.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -27,15 +33,21 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Category cat1 = new Category(null, "Electronics"); 
+        Category cat2 = new Category(null, "Books"); 
+        Category cat3 = new Category(null, "Computers"); 
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1); 
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2); 
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1); 
         
-        userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
-
 }
---
+++ src/main/java/com/vhaporfiro/ProjetoWebServices/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.vhaporfiro.ProjetoWebServices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vhaporfiro.ProjetoWebServices.entities.Category;
+import com.vhaporfiro.ProjetoWebServices.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
