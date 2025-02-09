9becc5e963c9c23ec16fd93d50183273ff0f9d7d
Victor Hugo <vhaporfiro@gmail.com>
2025-02-09 07:57:22 -0300

    Payment, one-to-one association

--- src/main/java/com/vhaporfiro/ProjetoWebServices/config/TestConfig.java
+++ src/main/java/com/vhaporfiro/ProjetoWebServices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vhaporfiro.ProjetoWebServices.entities.Category;
 import com.vhaporfiro.ProjetoWebServices.entities.Order;
 import com.vhaporfiro.ProjetoWebServices.entities.OrderItem;
+import com.vhaporfiro.ProjetoWebServices.entities.Payment;
 import com.vhaporfiro.ProjetoWebServices.entities.Product;
 import com.vhaporfiro.ProjetoWebServices.entities.User;
 import com.vhaporfiro.ProjetoWebServices.entities.enums.OrderStatus;
@@ -70,7 +71,7 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1); 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2); 
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1); 
         
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1); 
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
